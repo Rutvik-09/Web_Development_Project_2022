48294d2ceb7d6c8b010beec6ea27515d1d1ef516
Vivekkumar Patel <vv662564@dal.ca>
2022-06-15 01:00:25 -0300

    user profile page

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,50 +1,72 @@
 import "./Header.css";
 import rentlogo from "../../assets/images/rent.png";
+import user from "../../assets/images/user.png";
 
 const Header = () => {
   return (
-    
-      <nav class="navbar  navbar-expand-lg navbar-light bg-light  sticky-top" >
-        <a class="navbar-brand" href="/signup">
-      {/* rent logo has been taken from the "https://www.flaticon.com/free-icon/for-rent_1009805" */}
+    <nav class="navbar  navbar-expand-lg navbar-light bg-light  sticky-top">
+      <a class="navbar-brand" href="/signup">
+        {/* rent logo has been taken from the "https://www.flaticon.com/free-icon/for-rent_1009805" */}
         <img src={rentlogo} alt="For rent" className="rent-logo" />
-        </a>
-        <button
-          class="navbar-toggler"
-          aria-controls="signup"
-          aria-expanded="false"
-          aria-label="Signup navigation"
-          type="button"
-          data-toggle="collapse"
-          data-target="#signup"
-        
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="signup">
-          
+      </a>
+      <button
+        class="navbar-toggler"
+        aria-controls="signup"
+        aria-expanded="false"
+        aria-label="Signup navigation"
+        type="button"
+        data-toggle="collapse"
+        data-target="#signup"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="signup">
         <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="/communityforum">
-                Community Forum
+          <li class="nav-item">
+            <a class="nav-link" href="/communityforum">
+              Community Forum
+            </a>
+          </li>
+        </ul>
+        <ul class="navbar-nav" style={{ marginLeft: "auto" }}>
+          <li class="nav-item">
+            <a class="nav-link" href="/signin">
+              Login
+            </a>
+          </li>
+          <li class="nav-item  active">
+            <a class="nav-link" href="/signup">
+              Signup
+            </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="true"
+            >
+              <img src={user} alt="user profile pic" className="user-profile-pic" />
+            </a>
+            <div class="dropdown-menu custom-css" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="/userprofile">
+                Profile
               </a>
-            </li>
-          </ul>
-          <ul class="navbar-nav" style={{ marginLeft: 'auto' }}>
-            <li class="nav-item">
-              <a class="nav-link" href="/signin">
-                Login
+              <a class="dropdown-item" href="/signup">
+                Signout
               </a>
-            </li>
-            <li class="nav-item  active" >
-              <a class="nav-link" href="/signup">
-                Signup
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    
+              {/* <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">
+                Something else here
+              </a> */}
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
